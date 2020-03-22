9400d05bedbe8457ea8f9b7ab50e3afa7fb994d2
jakob153 <guenayjakob@gmail.com>
2020-03-22 04:45:52 +0100

    add missing files

---
+++ src/refreshToken.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt, { VerifyErrors } from 'jsonwebtoken';
+
+import { User } from './entity/User';
+
+const secret = process.env.SECRET as string;
+
+export const refreshToken = async (req: Request, res: Response, next: NextFunction) => {
+  if (!(req.cookies['auth_token'] && req.cookies['refresh_token'])) {
+    return res.status(400).send('No Auth/Refresh Token Provided');
+  }
+
+  const refreshToken = req.cookies['refresh_token'];
+
+  jwt.verify(refreshToken, secret, async (error: VerifyErrors) => {
+    if (error.name === 'TokenExpiredError') {
+      return res.redirect(
+        `${process.env.REACT_APP}/login?redirectMessage="Session Expired. Please Log In Again"`
+      );
+    }
+
+    if (error) {
+      return res.status(400).send('Token Invalid/Expired');
+    }
+
+    const user = await User.findOne({ refreshToken });
+
+    if (!user) {
+      return res.status(400).send('Token Invalid /Expired');
+    }
+
+    const authToken = jwt.sign({ sub: user.id }, secret, {
+      expiresIn: 10
+    });
+
+    req.cookies['auth_token'] = authToken;
+  });
+
+  return next();
+};
