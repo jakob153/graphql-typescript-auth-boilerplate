7af5084eed0c5306118433f99f977e91f3872894
jakob153 <guenayjakob@gmail.com>
2020-05-15 19:04:34 +0200

    remove eslint

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'no-console': ['error', { allow: ['error'] }],
-  },
-};
